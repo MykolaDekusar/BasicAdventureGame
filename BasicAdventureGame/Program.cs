ecd1481a2a914bba6d9d4a1b2f4918b283a85793
MykolaDekusar <ndekusar97@gmail.com>
2025-07-27 18:17:32 +0200

    Added more paths

--- BasicAdventureGame/Program.cs
+++ BasicAdventureGame/Program.cs
@@ -6,29 +6,52 @@ Thread.Sleep(1000);
 Console.WriteLine("Inserisci il tuo nome: ");
 string userName = Console.ReadLine();
 Console.WriteLine($"\nBenvenuto nel gico {userName}, scegli la tua classe tra Guerriro, Arciere, Mago, Curatore.");
-string userClass = Console.ReadLine();
-Console.WriteLine($"\nE` da tanto tempo che Narzelot aspettava un prode {userClass} come te {userName}... \nTi ritrovi in una foresta buia e hai 2 strade davanti a te... \nCosa sceglierai? (Destra / Sinistra)");
-string choice1 = Console.ReadLine().ToLower();
+string userClass = Console.ReadLine().ToLower().Trim();
+
+while (userClass != "guerriero" && userClass != "arciere" && userClass != "mago" && userClass != "curatore")
+{
+    Console.WriteLine("Scegli una tra le classi disponibili...");
+    userClass = Console.ReadLine().ToLower().Trim();
+};
+//Inizializziamo il loop del gioco
+
+Console.WriteLine($"\nE` da tanto tempo che Narzelot aspettava un prode {userClass} come te {userName}... \nTi ritrovi in una foresta buia e hai 2 strade davanti a te... \nCosa sceglierai? \n(Destra / Sinistra)");
+string choice1 = Console.ReadLine().ToLower().Trim();
 //verifico che la scelta sia giusta
-do
-{
-    Console.WriteLine("Scegli Destra o Sinistra");
-    choice1 = Console.ReadLine().ToLower();
-} while (choice1 != "destra" && choice1 != "sinistra");
+while (choice1 != "destra" && choice1 != "sinistra"){
+    Console.WriteLine("\nScegli Destra o Sinistra");
+    choice1 = Console.ReadLine().ToLower().Trim();
+}
 
-if(choice1 == "right")
+if(choice1 == "destra")
 {
-    Console.WriteLine("Hai scelto di proseguire a Destra e ti sei imbattuto in un lupo mannaro... \n La bestia non ti ha ancora visto... Cosa deciderai di fare? (Scappa / Combatti)");
-    string choice2 = Console.ReadLine().ToLower();
-    do
+    Console.WriteLine("\nHai scelto di proseguire a Destra e ti sei imbattuto in un lupo mannaro... \nLa bestia non ti ha ancora visto... Cosa deciderai di fare? \n(Scappa / Combatti)");
+    string choice2 = Console.ReadLine().ToLower().Trim();
+    while (choice2 != "scappa" && choice2 != "combatti"){
+        Console.WriteLine("\nScegli Scappa o Combatti");
+        choice2 = Console.ReadLine().ToLower().Trim();
+    }
+    if(choice2 == "scappa")
     {
-        Console.WriteLine("Scegli Scappa o Combatti");
-        choice2 = Console.ReadLine().ToLower();
-    } while (choice2 != "scappa" && choice2 != "combatti");
+        Console.WriteLine("\nHai cercato di sfuggire invano...\nIl fiuto della bestia ha percepito il tuo odore e non hai potuto fare nulla...\nCon un salto rapido ti ha raggiunto e ti ha staccato la testa...\n");
+        Thread.Sleep(3000);
+        Console.WriteLine("\nGame Over");
+        return;
+    }
+    if(choice2 == "combatti")
+    {
+        Console.WriteLine("\nPur sapendo che non hai possibilità contro il lupo mannaro hai comunque scelto di combattere?\n Davvero corraggioso da parte tua..." +
+            "\nO magari non vedi l'ora di morire e di lasciare questo mondo?" +
+            "\n");
+    }
 
+}else if (choice1 == "sinistra")
+{
+    Console.WriteLine("In Sviluppo...");
 }
 
 
 
 
-Console.ReadKey();
+
+    Console.ReadKey();
