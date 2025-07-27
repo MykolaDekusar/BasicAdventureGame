3a3612cb63f514ac2c21b94f9546438dc5befe26
MykolaDekusar <ndekusar97@gmail.com>
2025-07-27 18:04:49 +0200

    Added choice1 Right route path

--- BasicAdventureGame/Program.cs
+++ BasicAdventureGame/Program.cs
@@ -9,6 +9,24 @@ Console.WriteLine($"\nBenvenuto nel gico {userName}, scegli la tua classe tra Gu
 string userClass = Console.ReadLine();
 Console.WriteLine($"\nE` da tanto tempo che Narzelot aspettava un prode {userClass} come te {userName}... \nTi ritrovi in una foresta buia e hai 2 strade davanti a te... \nCosa sceglierai? (Destra / Sinistra)");
 string choice1 = Console.ReadLine().ToLower();
+//verifico che la scelta sia giusta
+do
+{
+    Console.WriteLine("Scegli Destra o Sinistra");
+    choice1 = Console.ReadLine().ToLower();
+} while (choice1 != "destra" && choice1 != "sinistra");
+
+if(choice1 == "right")
+{
+    Console.WriteLine("Hai scelto di proseguire a Destra e ti sei imbattuto in un lupo mannaro... \n La bestia non ti ha ancora visto... Cosa deciderai di fare? (Scappa / Combatti)");
+    string choice2 = Console.ReadLine().ToLower();
+    do
+    {
+        Console.WriteLine("Scegli Scappa o Combatti");
+        choice2 = Console.ReadLine().ToLower();
+    } while (choice2 != "scappa" && choice2 != "combatti");
+
+}
 
 
 
