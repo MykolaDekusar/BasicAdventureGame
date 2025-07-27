5be7e0c038171bae742e7f64b28baafbf7b2147a
MykolaDekusar <ndekusar97@gmail.com>
2025-07-27 17:58:35 +0200

    Initial setup of user name and class... Setup of first player choice.

--- BasicAdventureGame/Program.cs
+++ BasicAdventureGame/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿//Creiamo un piccolo gioco di ruolo in console
+using System.Reflection.Metadata;
+
+Console.WriteLine("Avvio del gioco...");
+Thread.Sleep(1000);
+Console.WriteLine("Inserisci il tuo nome: ");
+string userName = Console.ReadLine();
+Console.WriteLine($"\nBenvenuto nel gico {userName}, scegli la tua classe tra Guerriro, Arciere, Mago, Curatore.");
+string userClass = Console.ReadLine();
+Console.WriteLine($"\nE` da tanto tempo che Narzelot aspettava un prode {userClass} come te {userName}... \nTi ritrovi in una foresta buia e hai 2 strade davanti a te... \nCosa sceglierai? (Destra / Sinistra)");
+string choice1 = Console.ReadLine().ToLower();
+
+
+
+
+Console.ReadKey();
